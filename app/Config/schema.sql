ce6ccc7ca31c5ce1612e06b25e69b6d1512669ee
WVBR <webadmin@wvbr.com>
2012-09-26 01:25:04 -0400

    Updated schema sql

--- app/Config/schema.sql
+++ app/Config/schema.sql
@@ -1,7 +1,7 @@
 USE db433094351;
 
 # DROP TABLE album_covers;
-CREATE TABLE album_covers (
+CREATE TABLE IF NOT EXISTS album_covers (
 id int(11) unsigned primary key auto_increment NOT NULL,
 artist varchar(255) NOT NULL,
 album varchar(255) NOT NULL,
@@ -10,13 +10,13 @@ date_released DATE DEFAULT NULL,
 date_added DATE NOT NULL
 ) CHARACTER SET = utf8;
 
-CREATE TABLE shows (
+CREATE TABLE IF NOT EXISTS shows (
 id int(11) unsigned primary key auto_increment NOT NULL,
 name varchar(255) NOT NULL,
 date DATE NOT NULL
 ) CHARACTER SET = utf8;
 
-CREATE TABLE track_listings (
+CREATE TABLE IF NOT EXISTS track_listings (
 id int(11) unsigned primary key auto_increment NOT NULL,
 show_id int(11) unsigned NOT NULL,
 title varchar(255) NOT NULL,
