5596139cd1c10c8d665ddfe53203d215fdb7f84e
Parker Moore <parkrmoore@gmail.com>
2012-10-15 19:26:21 -0400

    Getting rid of scaffolding functions in views, using cake layout

--- app/Controller/AlbumCoversController.php
+++ app/Controller/AlbumCoversController.php
@@ -8,101 +8,12 @@ App::uses('AppController', 'Controller');
 class AlbumCoversController extends AppController {
 
 /**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->AlbumCover->recursive = 0;
-		$this->set('albumCovers', $this->paginate());
-	}
-
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		$this->AlbumCover->id = $id;
-		if (!$this->AlbumCover->exists()) {
-			throw new NotFoundException(__('Invalid album cover'));
-		}
-		$this->set('albumCover', $this->AlbumCover->read(null, $id));
-	}
-
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->AlbumCover->create();
-			if ($this->AlbumCover->save($this->request->data)) {
-				$this->Session->setFlash(__('The album cover has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The album cover could not be saved. Please, try again.'));
-			}
-		}
-	}
-
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		$this->AlbumCover->id = $id;
-		if (!$this->AlbumCover->exists()) {
-			throw new NotFoundException(__('Invalid album cover'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->AlbumCover->save($this->request->data)) {
-				$this->Session->setFlash(__('The album cover has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The album cover could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->AlbumCover->read(null, $id);
-		}
-	}
-
-/**
- * delete method
- *
- * @throws MethodNotAllowedException
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->AlbumCover->id = $id;
-		if (!$this->AlbumCover->exists()) {
-			throw new NotFoundException(__('Invalid album cover'));
-		}
-		if ($this->AlbumCover->delete()) {
-			$this->Session->setFlash(__('Album cover deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Album cover was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
-
-/**
  * admin_index method
  *
  * @return void
  */
 	public function admin_index() {
+        $this->layout = "cake";
 		$this->AlbumCover->recursive = 0;
 		$this->set('albumCovers', $this->paginate());
 	}
@@ -115,6 +26,7 @@ class AlbumCoversController extends AppController {
  * @return void
  */
 	public function admin_view($id = null) {
+        $this->layout = "cake";
 		$this->AlbumCover->id = $id;
 		if (!$this->AlbumCover->exists()) {
 			throw new NotFoundException(__('Invalid album cover'));
@@ -128,6 +40,7 @@ class AlbumCoversController extends AppController {
  * @return void
  */
 	public function admin_add() {
+        $this->layout = "cake";
 		if ($this->request->is('post')) {
 			$this->AlbumCover->create();
 			if ($this->AlbumCover->save($this->request->data)) {
@@ -147,6 +60,7 @@ class AlbumCoversController extends AppController {
  * @return void
  */
 	public function admin_edit($id = null) {
+        $this->layout = "cake";
 		$this->AlbumCover->id = $id;
 		if (!$this->AlbumCover->exists()) {
 			throw new NotFoundException(__('Invalid album cover'));
@@ -172,6 +86,7 @@ class AlbumCoversController extends AppController {
  * @return void
  */
 	public function admin_delete($id = null) {
+        $this->layout = "cake";
 		if (!$this->request->is('post')) {
 			throw new MethodNotAllowedException();
 		}
--- app/Controller/ShowsController.php
+++ app/Controller/ShowsController.php
@@ -8,101 +8,12 @@ App::uses('AppController', 'Controller');
 class ShowsController extends AppController {
 
 /**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->Show->recursive = 1;
-		$this->set('shows', $this->Show->find('all', array('order' => array('Show.date DESC'))));
-	}
-
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		$this->Show->id = $id;
-		if (!$this->Show->exists()) {
-			throw new NotFoundException(__('Invalid show'));
-		}
-		$this->set('show', $this->Show->read(null, $id));
-	}
-
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Show->create();
-			if ($this->Show->save($this->request->data)) {
-				$this->Session->setFlash(__('The show has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The show could not be saved. Please, try again.'));
-			}
-		}
-	}
-
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		$this->Show->id = $id;
-		if (!$this->Show->exists()) {
-			throw new NotFoundException(__('Invalid show'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Show->save($this->request->data)) {
-				$this->Session->setFlash(__('The show has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The show could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Show->read(null, $id);
-		}
-	}
-
-/**
- * delete method
- *
- * @throws MethodNotAllowedException
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Show->id = $id;
-		if (!$this->Show->exists()) {
-			throw new NotFoundException(__('Invalid show'));
-		}
-		if ($this->Show->delete()) {
-			$this->Session->setFlash(__('Show deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Show was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
-
-/**
  * admin_index method
  *
  * @return void
  */
 	public function admin_index() {
+        $this->layout = "cake";
 		$this->Show->recursive = 0;
 		$this->set('shows', $this->paginate());
 	}
@@ -115,6 +26,7 @@ class ShowsController extends AppController {
  * @return void
  */
 	public function admin_view($id = null) {
+        $this->layout = "cake";
 		$this->Show->id = $id;
 		if (!$this->Show->exists()) {
 			throw new NotFoundException(__('Invalid show'));
@@ -128,6 +40,7 @@ class ShowsController extends AppController {
  * @return void
  */
 	public function admin_add() {
+        $this->layout = "cake";
 		if ($this->request->is('post')) {
 			$this->Show->create();
 			if ($this->Show->save($this->request->data)) {
@@ -147,6 +60,7 @@ class ShowsController extends AppController {
  * @return void
  */
 	public function admin_edit($id = null) {
+        $this->layout = "cake";
 		$this->Show->id = $id;
 		if (!$this->Show->exists()) {
 			throw new NotFoundException(__('Invalid show'));
@@ -172,6 +86,7 @@ class ShowsController extends AppController {
  * @return void
  */
 	public function admin_delete($id = null) {
+        $this->layout = "cake";
 		if (!$this->request->is('post')) {
 			throw new MethodNotAllowedException();
 		}
